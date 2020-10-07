85691d07a18f100019bf58772b26ca735ad82647
J. Steen Hoyer <j.s.hoyer@wustl.edu>
2020-10-07 12:24:55 -0400

    Add shell script for downloading data from SRA
    Had to edit script from SRA Explorer
    more than I expected.
    Manually added numbers 01 to 12,
    to counteract the arbitrariness in the SRX numbers.
    Decided it was worth having R1/R2 in the file names
    but not 001.

---
+++ 005_download-data-from-SRA.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/028/SRR12354428/SRR12354428_1.fastq.gz -o ACMV_DNA-A_03_SRR12354428_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/028/SRR12354428/SRR12354428_2.fastq.gz -o ACMV_DNA-A_03_SRR12354428_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/031/SRR12354431/SRR12354431_1.fastq.gz -o ACMV_DNA-A_02_SRR12354431_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/031/SRR12354431/SRR12354431_2.fastq.gz -o ACMV_DNA-A_02_SRR12354431_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/032/SRR12354432/SRR12354432_1.fastq.gz -o ACMV_DNA-A_01_SRR12354432_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/032/SRR12354432/SRR12354432_2.fastq.gz -o ACMV_DNA-A_01_SRR12354432_R2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/025/SRR12354425/SRR12354425_1.fastq.gz -o ACMV_DNA-B_06_SRR12354425_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/025/SRR12354425/SRR12354425_2.fastq.gz -o ACMV_DNA-B_06_SRR12354425_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/026/SRR12354426/SRR12354426_1.fastq.gz -o ACMV_DNA-B_05_SRR12354426_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/026/SRR12354426/SRR12354426_2.fastq.gz -o ACMV_DNA-B_05_SRR12354426_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/027/SRR12354427/SRR12354427_1.fastq.gz -o ACMV_DNA-B_04_SRR12354427_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/027/SRR12354427/SRR12354427_2.fastq.gz -o ACMV_DNA-B_04_SRR12354427_R2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/022/SRR12354422/SRR12354422_1.fastq.gz -o EACMCV_DNA-A_09_SRR12354422_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/022/SRR12354422/SRR12354422_2.fastq.gz -o EACMCV_DNA-A_09_SRR12354422_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/023/SRR12354423/SRR12354423_1.fastq.gz -o EACMCV_DNA-A_08_SRR12354423_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/023/SRR12354423/SRR12354423_2.fastq.gz -o EACMCV_DNA-A_08_SRR12354423_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/024/SRR12354424/SRR12354424_1.fastq.gz -o EACMCV_DNA-A_07_SRR12354424_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/024/SRR12354424/SRR12354424_2.fastq.gz -o EACMCV_DNA-A_07_SRR12354424_R2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/021/SRR12354421/SRR12354421_1.fastq.gz -o EACMCV_DNA-B_10_SRR12354421_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/021/SRR12354421/SRR12354421_2.fastq.gz -o EACMCV_DNA-B_10_SRR12354421_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/029/SRR12354429/SRR12354429_1.fastq.gz -o EACMCV_DNA-B_12_SRR12354429_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/029/SRR12354429/SRR12354429_2.fastq.gz -o EACMCV_DNA-B_12_SRR12354429_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/030/SRR12354430/SRR12354430_1.fastq.gz -o EACMCV_DNA-B_11_SRR12354430_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR123/030/SRR12354430/SRR12354430_2.fastq.gz -o EACMCV_DNA-B_11_SRR12354430_R2.fastq.gz
