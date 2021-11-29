69b08c12c76e9745ef597b6b916369aecfd1f939
J. Steen Hoyer <j.s.hoyer@wustl.edu>
2021-11-29 15:29:49 -0500

    Shell script for downloading Illumina data from SRA

---
+++ 005_download-data-from-SRA.sh
@@ -0,0 +1,30 @@
+﻿#!/usr/bin/env bash
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/076/SRR16995176/SRR16995176_1.fastq.gz -o plant01_SRR16995176_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/076/SRR16995176/SRR16995176_2.fastq.gz -o plant01_SRR16995176_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/063/SRR16995163/SRR16995163_1.fastq.gz -o plant10_SRR16995163_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/063/SRR16995163/SRR16995163_2.fastq.gz -o plant10_SRR16995163_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/069/SRR16995169/SRR16995169_1.fastq.gz -o plant04_SRR16995169_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/069/SRR16995169/SRR16995169_2.fastq.gz -o plant04_SRR16995169_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/068/SRR16995168/SRR16995168_1.fastq.gz -o plant05_SRR16995168_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/068/SRR16995168/SRR16995168_2.fastq.gz -o plant05_SRR16995168_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/075/SRR16995175/SRR16995175_1.fastq.gz -o plant02_SRR16995175_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/075/SRR16995175/SRR16995175_2.fastq.gz -o plant02_SRR16995175_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/070/SRR16995170/SRR16995170_1.fastq.gz -o plant03_SRR16995170_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/070/SRR16995170/SRR16995170_2.fastq.gz -o plant03_SRR16995170_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/065/SRR16995165/SRR16995165_1.fastq.gz -o plant08_SRR16995165_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/065/SRR16995165/SRR16995165_2.fastq.gz -o plant08_SRR16995165_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/064/SRR16995164/SRR16995164_1.fastq.gz -o plant09_SRR16995164_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/064/SRR16995164/SRR16995164_2.fastq.gz -o plant09_SRR16995164_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/074/SRR16995174/SRR16995174_1.fastq.gz -o plant11_SRR16995174_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/074/SRR16995174/SRR16995174_2.fastq.gz -o plant11_SRR16995174_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/073/SRR16995173/SRR16995173_1.fastq.gz -o plasmid_pNSB1101_SRR16995173_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/073/SRR16995173/SRR16995173_2.fastq.gz -o plasmid_pNSB1101_SRR16995173_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/066/SRR16995166/SRR16995166_1.fastq.gz -o plant07_SRR16995166_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/066/SRR16995166/SRR16995166_2.fastq.gz -o plant07_SRR16995166_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/072/SRR16995172/SRR16995172_1.fastq.gz -o plasmid_pCPCbLCVA.003_SRR16995172_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/072/SRR16995172/SRR16995172_2.fastq.gz -o plasmid_pCPCbLCVA.003_SRR16995172_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/071/SRR16995171/SRR16995171_1.fastq.gz -o plasmid_pCPCbLCVB.002_SRR16995171_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/071/SRR16995171/SRR16995171_2.fastq.gz -o plasmid_pCPCbLCVB.002_SRR16995171_R2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/067/SRR16995167/SRR16995167_1.fastq.gz -o plant06_SRR16995167_R1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR169/067/SRR16995167/SRR16995167_2.fastq.gz -o plant06_SRR16995167_R2.fastq.gz
